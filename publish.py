992898f55f80ac74903da191a2bc3ff839e326f6
upadhyaynikita <99130320+upadhyaynikita@users.noreply.github.com>
2024-04-16 14:18:19 +0530

    Update publish.py

--- publish.py
+++ publish.py
@@ -38,41 +38,41 @@ def publish_data():
         st.error(f"An error occurred: {str(e)}")
 
 # Streamlit UI
-def main():
-    # Button to trigger data publishing process
-    if st.button("Publish", help="Click to publish data", key="publish_button"):
-        publish_data()
+# def main():
+#     # Button to trigger data publishing process
+#     if st.button("Publish", help="Click to publish data", key="publish_button"):
+#         publish_data()
 
-    # Injecting CSS to position the button in the bottom right corner and change its color
-    button_style = """
-    <style>
-    .stButton button {
-        position: fixed;
-        bottom: 20px;
-        right: 20px;
-        background-color: #2E8B57; /* Sea green */
-    }
-    .stButton button:hover {
-        background-color: #3CB371; /* Light sea green */
-    }
-    </style>
-    """
-    st.markdown(button_style, unsafe_allow_html=True)
+#     # Injecting CSS to position the button in the bottom right corner and change its color
+#     button_style = """
+#     <style>
+#     .stButton button {
+#         position: fixed;
+#         bottom: 20px;
+#         right: 20px;
+#         background-color: #2E8B57; /* Sea green */
+#     }
+#     .stButton button:hover {
+#         background-color: #3CB371; /* Light sea green */
+#     }
+#     </style>
+#     """
+#     st.markdown(button_style, unsafe_allow_html=True)
 
-    # Show success message below the button when the process is completed successfully
-    if st.session_state.get("publish_success", False):
-        success_message_style = """
-        <style>
-        .success-message {
-            position: fixed;
-            bottom: 50px; /* Adjusted position */
-            right: 20px;
-            color: #2E8B57;
-        }
-        </style>
-        """
-        st.markdown(success_message_style, unsafe_allow_html=True)
-        st.markdown('<p class="success-message">Process completed successfully!</p>', unsafe_allow_html=True)
+#     # Show success message below the button when the process is completed successfully
+#     if st.session_state.get("publish_success", False):
+#         success_message_style = """
+#         <style>
+#         .success-message {
+#             position: fixed;
+#             bottom: 50px; /* Adjusted position */
+#             right: 20px;
+#             color: #2E8B57;
+#         }
+#         </style>
+#         """
+#         st.markdown(success_message_style, unsafe_allow_html=True)
+#         st.markdown('<p class="success-message">Process completed successfully!</p>', unsafe_allow_html=True)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
